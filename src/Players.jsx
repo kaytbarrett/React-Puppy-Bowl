13cf2217cd2faa398c6eb0937b3c4cf99292f50a
Katie Barrett <katiebarrett@Katies-MacBook-Air.local>
2023-10-02 19:42:52 -0600

    form created, still in progress

--- src/Players.jsx
+++ src/Players.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useFetchPlayersQuery } from './puppyBowlApi';
 import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
+import SignUpForm from './SignUpForm';
 
 
 
@@ -18,28 +19,34 @@ const Players = () => {
     }
 
     return (
-        <div className="players">
-            <div className="intro">
-                <h1>Welcome to the Puppy Bowl Event!</h1>
-                <p>Come and join us for a fun-filled day of adorable puppy football!</p>
-                <p>Get ready to meet the talented pups competing in this year's Puppy Bowl... and here they are:</p>
-            </div>
-
-            {data.data.players.map((player) => (
-                <div key={player.id} className="player-card">
-                    <img className="player-image" src={player.imageUrl} alt={player.name} />
-
-                    <div className="player-details">
-                        <h2> {player.name} </h2>
-                        <h2 className="breed">Breed: {player.breed} </h2>
-                        <h2 className="status">Status: {player.status} </h2>
-                    </div>
-                    <Link to={`/player/${player.id}`}>
-                        <Button color="secondary" key={player.id} > See Details
-                        </Button>
-                    </Link>
+        <div>
+            <div className="players">
+                <div className="intro">
+                    <h1>Welcome to the Puppy Bowl Event!</h1>
+                    <p>Come and join us for a fun-filled day of adorable puppy football!</p>
+                    <p>Get ready to meet the talented pups competing in this year's Puppy Bowl... and here they are:</p>
                 </div>
-            ))}
+                <div className="form">
+                <SignUpForm />
+                </div>
+                {data.data.players.map((player) => (
+                    <div key={player.id} className="player-card">
+                        <img className="player-image" src={player.imageUrl} alt={player.name} />
+
+                        <div className="player-details">
+                            <h2> {player.name} </h2>
+                            <h2 className="breed">Breed: {player.breed} </h2>
+                            <h2 className="status">Status: {player.status} </h2>
+                        </div>
+                        <Link to={`/player/${player.id}`}>
+                            <Button color="secondary" key={player.id} > See Details
+                            </Button>
+                        </Link>
+
+                    </div>
+
+                ))}
+            </div>
         </div>
     );
 };
